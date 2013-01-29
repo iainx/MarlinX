58b313bd4d9d5e29bd53ab9957d942b9fbb9e8fa
iain <iain@Tonawanda.local>
2013-01-29 19:33:07 +1100

    Initial Commit

---
+++ Marlin/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Marlin
+//
+//  Created by iain on 29/01/2013.
+//  Copyright (c) 2013 iain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
