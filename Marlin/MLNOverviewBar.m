6545f3b21a70baf908c880ff3e8469292c1f2c61
iain <iain@tonawanda.home>
2013-04-02 18:14:09 +0100

    Draw the sample in the overview

--- Marlin/MLNOverviewBar.m
+++ Marlin/MLNOverviewBar.m
@@ -8,8 +8,11 @@
 
 #import "MLNOverviewBar.h"
 #import "MLNSample.h"
+#import "MLNSample+Drawing.h"
 
-@implementation MLNOverviewBar
+@implementation MLNOverviewBar {
+    NSMutableArray *_channelMasks;
+}
 
 @synthesize sample = _sample;
 
@@ -22,13 +25,97 @@ static void *sampleContext = &sampleContext;
         // Initialization code here.
     }
     
+    _channelMasks = [[NSMutableArray alloc] init];
+    
+    [self setContentCompressionResistancePriority:NSLayoutPriorityRequired
+                                   forOrientation:NSLayoutConstraintOrientationVertical];
+    [self setContentHuggingPriority:NSLayoutPriorityRequired
+                     forOrientation:NSLayoutConstraintOrientationVertical];
     return self;
+}
+
+#pragma mark - Layout
+
+- (NSSize)intrinsicContentSize
+{
+    CGFloat height;
+    
+    if ([_sample numberOfChannels] < 2) {
+        height = 24;
+    } else {
+        height = 12 * [_sample numberOfChannels];
+    }
+    
+    DDLogVerbose(@"Overview height: %f", height);
+    return NSMakeSize(NSViewNoInstrinsicMetric, height);
 }
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    [[NSColor redColor] set];
-    NSRectFill(dirtyRect);
+    CGContextRef context = [[NSGraphicsContext currentContext] graphicsPort];
+    NSRect bounds = [self bounds];
+    NSRect channelRect = bounds;
+    CGFloat channelHeight = bounds.size.height / [_sample numberOfChannels];
+    
+    channelRect.size.height = (channelHeight - 1);
+    
+    for (int channel = 0; channel < [_sample numberOfChannels]; channel++) {
+        CGContextSaveGState(context);
+        CGImageRef channelMask = (CGImageRef)CFBridgingRetain([_channelMasks objectAtIndex:channel]);
+        
+        channelRect.origin.y = (bounds.size.height - (channelHeight * (channel + 1))) + 1;
+        
+        CGContextClipToMask(context, channelRect, channelMask);
+        
+        [[NSColor redColor] setFill];
+        NSRectFill(channelRect);
+        CGContextRestoreGState(context);
+    }
+    
+    [[NSColor blackColor] set];
+    [[NSBezierPath bezierPathWithRect:bounds] stroke];
+}
+
+- (CGContextRef)createMaskContextForRect:(NSRect)scaledRect
+{
+    CGContextRef maskContext;
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceGray();
+    
+    maskContext = CGBitmapContextCreate(NULL,
+                                        scaledRect.size.width,
+                                        scaledRect.size.height,
+                                        8,
+                                        scaledRect.size.width,
+                                        colorspace,
+                                        0);
+    CGColorSpaceRelease(colorspace);
+    
+    return maskContext;
+}
+
+- (void)createChannelMasks
+{
+    NSRect channelRect = [self convertRectToBacking:[self bounds]];
+    NSUInteger framesPerPixel;
+    
+    channelRect.size.height /= [_sample numberOfChannels];
+    //channelRect = NSInsetRect(channelRect, 0, 1);
+    
+    framesPerPixel = [_sample numberOfFrames] / channelRect.size.width;
+
+    [_channelMasks removeAllObjects];
+    for (int channel = 0; channel < [_sample numberOfChannels]; channel++) {
+        CGContextRef maskContext = [self createMaskContextForRect:channelRect];
+        CGImageRef channelMask;
+        
+        [_sample drawWaveformInContext:maskContext
+                         channelNumber:channel
+                                  rect:channelRect
+                    withFramesPerPixel:framesPerPixel];
+        channelMask = CGBitmapContextCreateImage(maskContext);
+        
+        [_channelMasks addObject:CFBridgingRelease(channelMask)];
+    }
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath
@@ -41,25 +128,17 @@ static void *sampleContext = &sampleContext;
     }
     
     if ([keyPath isEqualToString:@"numberOfChannels"]) {
+        [self createChannelMasks];
         [self invalidateIntrinsicContentSize];
         [self setNeedsDisplay:YES];
         return;
     }
-}
-#pragma mark - Layout
-
-- (NSSize)intrinsicContentSize
-{
-    CGFloat height;
     
-    if ([_sample numberOfChannels] == 0) {
-        height = 24;
-    } else {
-        height = 12 * [_sample numberOfChannels];
+    if ([keyPath isEqualToString:@"loaded"]) {
+        [self invalidateIntrinsicContentSize];
+        [self setNeedsDisplay:YES];
+        return;
     }
-    
-    DDLogVerbose(@"Overview height: %f", height);
-    return NSMakeSize(NSViewNoInstrinsicMetric, height);
 }
 
 #pragma mark - Accessors
@@ -77,6 +156,7 @@ static void *sampleContext = &sampleContext;
                  context:sampleContext];
     
     if ([_sample isLoaded]) {
+        [self createChannelMasks];
         [self invalidateIntrinsicContentSize];
         [self setNeedsDisplay:YES];
     }
